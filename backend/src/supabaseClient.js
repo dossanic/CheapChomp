d1f1ac835446b7d80ef2a068b4d530ff86069cfe
hahuynh18 <huynh18@sheridancollege.ca>
2026-05-28 17:58:59 -0400

    Delete backend/src directory

--- backend/src/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config({ path: "../.env" });
-const { createClient } = require("@supabase/supabase-js");
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
-);
-
-module.exports = supabase;
